38cc6ee6b69b4d8d0699177fee211dffa30a9ec1
Nicholas Saarela <nicholas.saarela@gmail.com>
2024-03-04 13:45:36 +0200

    Zero warnings and made engine to static lib

--- premake5.lua
+++ premake5.lua
@@ -24,3 +24,16 @@ project "ImGui"
 
     filter "system:windows"
         systemversion "latest"
+
+        
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+        
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
+
+    filter "configurations:Dist"
+        runtime "Release"
+        optimize "on"
